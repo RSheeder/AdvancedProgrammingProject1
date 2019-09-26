4797bf5e0ed1c62bbdd91d80c3baaff0ea2f2880
Richard <rjs53@students.uwf.edu>
2019-09-26 17:37:28 -0500

    Project 1

--- Project 1/Start/testDB/Startup.bat
+++ Project 1/Start/testDB/Startup.bat
@@ -1,6 +1,6 @@
 @echo off
 REM Set path to default java 11 install location
-set PATH=C:\Program Files\Java\jdk-11.0.4\bin
+REM set PATH=C:\Program Files\Java\jdk-11.0.4\bin
 
 set CLASSPATH=%cd%\lib\derby.jar;%cd%\lib\derbytools.jar;%cd%/lib/derbyoptionaltools.jar;%cd%
 javac TestDB.java
